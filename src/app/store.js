49ec671aabab093e272a5b7441f4d4acbdb7baf5
chaitanya005 <muvvalachaitanya05@gmail.com>
2022-07-07 16:02:20 +0530

    inital commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import TodoReducer from "../features/todos/Todos";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     todos: TodoReducer,
   },
 });
